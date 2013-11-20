d4a7b433fdc71b7beea5090ce5b99cbf074be8ec
Dylan Whichard <dylan@whichard.com>
2013-11-19 23:03:25 -0500

    Add beginnings of data server

--- TankNavigator/src/com/dylan/whichard/tanknavigator/SensorListenerService.java
+++ TankNavigator/src/com/dylan/whichard/tanknavigator/SensorListenerService.java
@@ -1,5 +1,8 @@
 package com.dylan.whichard.tanknavigator;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import android.app.Service;
 import android.content.Intent;
 import android.hardware.Sensor;
@@ -11,6 +14,13 @@ import android.os.IBinder;
 import android.util.Log;
 
 public class SensorListenerService extends Service implements SensorEventListener {
+	public static boolean RUNNING = false;
+	
+	public interface TankDataListener {
+		public void onAccelerationChanged(double x, double y, double z);
+		public void onLocationChanged();	// FIXME
+		public void onRotationChanged(double x, double y, double z);
+	}
 	
 	public class SensorListenerBinder extends Binder {
 		SensorListenerService getService() {
@@ -29,15 +39,22 @@ public class SensorListenerService extends Service implements SensorEventListene
 	private Sensor compass;
 	private final IBinder binder = new SensorListenerBinder();
 	
+	private Set<TankDataListener> listeners;
+	
 	@Override
 	public void onCreate() {
+		RUNNING = true;
+		
+		android.os.Debug.waitForDebugger();
+		
+		listeners = new HashSet<TankDataListener>();
+		
 		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 		accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 		compass = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
 		
-		
-		sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
-		sensorManager.registerListener(this, compass, SensorManager.SENSOR_DELAY_GAME);
+		sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+		sensorManager.registerListener(this, compass, SensorManager.SENSOR_DELAY_FASTEST);
 	}
 
 	@Override
@@ -49,9 +66,36 @@ public class SensorListenerService extends Service implements SensorEventListene
 	public void onAccuracyChanged(Sensor sensor, int accuracy) {
 		Log.d("SensorListener", "Sensor " + sensor.getName() + " accuracy changed to" + accuracy);
 	}
+	
+	@Override
+	public void onDestroy() {
+		RUNNING = false;
+		sensorManager.unregisterListener(this);
+	}
+	
+	public void addListener(TankDataListener l) {
+		listeners.add(l);
+	}
+	
+	public void removeListener(TankDataListener l) {
+		listeners.remove(l);
+	}
 
 	@Override
 	public void onSensorChanged(SensorEvent event) {
-		Log.d("SensorListener", "Sensor event from " + event.sensor.getName());
+		switch(event.sensor.getType()) {
+		case Sensor.TYPE_ACCELEROMETER:
+			for (TankDataListener l : listeners) {
+				l.onAccelerationChanged(event.values[0], event.values[1], event.values[2]);
+			}
+			Log.d("SensorListenerService", String.format("Accelerometer: [%.2f, %.2f, %.2f]", event.values[0], event.values[1], event.values[2]));
+			break;
+		case Sensor.TYPE_ROTATION_VECTOR:
+			for (TankDataListener l : listeners) {
+				l.onRotationChanged(event.values[0], event.values[1], event.values[2]);
+			}
+			Log.d("SensorListenerService", String.format("Rotation: <%.3f, %.3f, %.3f>", event.values[0], event.values[1], event.values[2]));
+			break;
+		}
 	}
 }
--- TankNavigator/src/com/dylan/whichard/tanknavigator/TankNavigatorApplication.java
+++ TankNavigator/src/com/dylan/whichard/tanknavigator/TankNavigatorApplication.java
@@ -1,7 +1,75 @@
 package com.dylan.whichard.tanknavigator;
 
 import android.app.Application;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.util.Log;
 
 public class TankNavigatorApplication extends Application {
 	
+	private SensorListenerService sensorService;
+	private SocketListenerService socketService;
+	
+	private boolean sensorBound;
+	private boolean socketBound;
+	
+	private ServiceConnection sensorConnection = new ServiceConnection() {
+	    public void onServiceConnected(ComponentName className, IBinder service) {
+	        // This is called when the connection with the service has been
+	        // established, giving us the service object we can use to
+	        // interact with the service.  Because we have bound to a explicit
+	        // service that we know is running in our own process, we can
+	        // cast its IBinder to a concrete class and directly access it.
+	        Log.d("TankNavigatorApplication", "Connecting to SensorListenerService");
+	        sensorService = ((SensorListenerService.SensorListenerBinder)service).getService();
+	    }
+
+	    public void onServiceDisconnected(ComponentName className) {
+	        // This is called when the connection with the service has been
+	        // unexpectedly disconnected -- that is, its process crashed.
+	        // Because it is running in our same process, we should never
+	        // see this happen.
+	        sensorService = null;
+	        Log.d("TankNavigatorApplication", "Disconnecting from SensorListenerService");
+	    }
+	};
+	
+	private ServiceConnection socketConnection = new ServiceConnection() {
+		
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+	        socketService = null;
+		}
+		
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+	        socketService = ((SocketListenerService.SocketListenerBinder)service).getService();
+		}
+	};
+	
+	public void onCreate() {
+		doBindService();
+	}
+
+	void doBindService() {
+	    // Establish a connection with the service.  We use an explicit
+	    // class name because we want a specific service implementation that
+	    // we know will be running in our own process (and thus won't be
+	    // supporting component replacement by other applications).
+	    bindService(new Intent(this, 
+	            SensorListenerService.class), sensorConnection, Context.BIND_AUTO_CREATE);
+	    bindService(new Intent(this, SocketListenerService.class), sensorConnection, Context.BIND_AUTO_CREATE);
+	    sensorBound = true;
+	}
+
+	void doUnbindService() {
+	    if (sensorBound) {
+	        // Detach our existing connection.
+	        unbindService(sensorConnection);
+	        sensorBound = false;
+	    }
+	}
 }
